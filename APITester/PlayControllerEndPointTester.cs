f8490575925f71f823bcf8b5cdb9cd6a6ee5afc3
Krisztian Cseh <csehkrisztian@outlook.com>
2025-12-14 12:28:41 +0100

    EndPoint tesztek implementálása, unit teszt finomítása

---
+++ APITester/PlayControllerEndPointTester.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using RockPaperScissors_Bll.Enums;
+using RockPaperScissors_Bll.Models;
+using RockPaperScissors_Bll.Services;
+using RockPaperScissors_Dll.Enums;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace APITester
+{
+    public class ApiFactory : WebApplicationFactory<RockPaperScissors.Program>
+    {
+        private readonly GameChoice _playerChoice;
+        private readonly GameStatusModel _gameStatus;
+        private readonly UserModel _user;
+
+        public ApiFactory(GameChoice playerChoice, GameStatusModel gameStatus, UserModel user)
+        {
+            _playerChoice = playerChoice;
+            _gameStatus = gameStatus;
+            _user = user;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll<IUserService>();
+                services.RemoveAll<IPlayService>();
+
+                var userServiceMock = new Mock<IUserService>();
+                var playServiceMock = new Mock<IPlayService>();
+
+                userServiceMock.Setup(s => s.CheckUser("test", "123"))
+                    .Returns(_user);
+
+                userServiceMock.Setup(s => s.GetUser(_user.Id))
+                    .Returns(_user);
+
+                playServiceMock.Setup(s => s.Play(_playerChoice))
+                .Returns(_gameStatus);
+
+                userServiceMock.Setup(s => s.SetGameResult(_user.Id, _gameStatus.GameResult))
+                    .Callback<int, GameResult>((id, result) =>
+                    {
+                        switch (result)
+                        {
+                            case GameResult.Won: _user.Won++; break;
+                            case GameResult.Lost: _user.Lost++; break;
+                            case GameResult.Draw: _user.Draw++; break;
+                        }
+                    });
+
+                services.AddScoped(_ => userServiceMock.Object);
+                services.AddScoped(_ => playServiceMock.Object);
+            });
+        }
+    }
+
+    public class PlayControllerEndPointTester
+    {
+        [Theory]
+        [InlineData(GameChoice.Rock, GameResult.Won, GameChoice.Scissors, 1, 0, 0)]
+        [InlineData(GameChoice.Paper, GameResult.Lost, GameChoice.Scissors, 0, 1, 0)]
+        [InlineData(GameChoice.Scissors, GameResult.Draw, GameChoice.Scissors, 0, 0, 1)]
+        [InlineData(GameChoice.Rock, GameResult.Lost, GameChoice.Paper, 0, 1, 0)]
+        [InlineData(GameChoice.Paper, GameResult.Draw, GameChoice.Paper, 0, 0, 1)]
+        [InlineData(GameChoice.Scissors, GameResult.Won, GameChoice.Paper, 1, 0, 0)]
+        [InlineData(GameChoice.Rock, GameResult.Draw, GameChoice.Rock, 0, 0, 1)]
+        [InlineData(GameChoice.Paper, GameResult.Won, GameChoice.Rock, 1, 0, 0)]
+        [InlineData(GameChoice.Scissors, GameResult.Lost, GameChoice.Rock, 0, 1, 0)]
+        public async Task Play_Endpoint_ReturnsExpected(GameChoice playerChoice, GameResult gameResult, GameChoice opponentChoice, int won, int lost, int draw)
+        {
+            var user = new UserModel
+            {
+                Id = 1,
+                Name = "test",
+                Password = "123",
+                Won = 0,
+                Lost = 0,
+                Draw = 0
+            };
+
+            var factory = new ApiFactory(
+                playerChoice, 
+                new GameStatusModel { GameResult = gameResult, OpponentChoice = opponentChoice }, 
+                user);
+
+            var client = factory.CreateClient();
+
+            var resp = await client.GetAsync($"/api/Play?userName=test&password=123&choice={playerChoice}");
+            resp.EnsureSuccessStatusCode();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            options.Converters.Add(new JsonStringEnumConverter());
+
+            var value = await resp.Content.ReadFromJsonAsync<ApiGameStatusModel>(options);
+
+            Assert.NotNull(value);
+            Assert.Equal("Ok.", value!.Message);
+
+            Assert.NotNull(value.GameStatus);
+            Assert.Equal(gameResult, value.GameStatus!.GameResult);
+            Assert.Equal(opponentChoice, value.GameStatus!.OpponentChoice);
+
+            Assert.NotNull(value.UserStats);
+            Assert.Equal(won, value.UserStats!.Won);
+            Assert.Equal(lost, value.UserStats!.Lost);
+            Assert.Equal(draw, value.UserStats!.Draw);
+        }
+    }
+}
--- APITester/PlayControllerTester.cs
+++ APITester/PlayControllerTester.cs
@@ -105,8 +105,17 @@ namespace APITester
             Assert.Equal("Hibás választás! (Rock, Paper, Scissors)", result.Message);
         }
 
-        [Fact]
-        public void Get_ValidUser_Rock_ReturnsGameResult()
+        [Theory]
+        [InlineData(GameChoice.Rock, GameResult.Won, GameChoice.Scissors, 1, 0, 0)]
+        [InlineData(GameChoice.Paper, GameResult.Lost, GameChoice.Scissors, 0, 1, 0)]
+        [InlineData(GameChoice.Scissors, GameResult.Draw, GameChoice.Scissors, 0, 0, 1)]
+        [InlineData(GameChoice.Rock, GameResult.Lost, GameChoice.Paper, 0, 1, 0)]
+        [InlineData(GameChoice.Paper, GameResult.Draw, GameChoice.Paper, 0, 0, 1)]
+        [InlineData(GameChoice.Scissors, GameResult.Won, GameChoice.Paper, 1, 0, 0)]
+        [InlineData(GameChoice.Rock, GameResult.Draw, GameChoice.Rock, 0, 0, 1)]
+        [InlineData(GameChoice.Paper, GameResult.Won, GameChoice.Rock, 1, 0, 0)]
+        [InlineData(GameChoice.Scissors, GameResult.Lost, GameChoice.Rock, 0, 1, 0)]
+        public void Get_ValidUser_ReturnsGameResult(GameChoice playerChoice, GameResult gameResult, GameChoice opponentChoice, int won, int lost, int draw)
         {
             // Arrange
             var user = new UserModel
@@ -125,19 +134,33 @@ namespace APITester
             userServiceMock.Setup(s => s.CheckUser("test", "123"))
                 .Returns(user);
 
-            userServiceMock.Setup(s => s.GetUser(1))
+            userServiceMock.Setup(s => s.GetUser(user.Id))
                 .Returns(user);
 
-            playServiceMock.Setup(s => s.Play(GameChoice.Rock))
+            playServiceMock.Setup(s => s.Play(playerChoice))
                 .Returns(new GameStatusModel
                 {
-                    GameResult = GameResult.Won
+                    GameResult = gameResult,
+                    OpponentChoice = opponentChoice
                 });
 
-            userServiceMock.Setup(s => s.SetGameResult(1, GameResult.Won))
+            userServiceMock.Setup(s => s.SetGameResult(user.Id, gameResult))
                 .Callback(() =>
                 {
-                    user.Won++;
+                    switch (gameResult)
+                    {
+                        case GameResult.Won:
+                            user.Won = won;
+                            break;
+                        case GameResult.Lost:
+                            user.Lost = lost;
+                            break;
+                        case GameResult.Draw:
+                            user.Draw = draw;
+                            break;
+                        default:
+                            break;
+                    }
                 });
 
             var controller = new PlayController(
@@ -146,15 +169,16 @@ namespace APITester
             );
 
             // Act
-            var result = controller.Get("test", "123", "Rock");
+            var result = controller.Get("test", "123", playerChoice.ToString());
 
             // Assert
             Assert.NotNull(result.GameStatus);
             Assert.NotNull(result.UserStats);
-            Assert.Equal(GameResult.Won, result.GameStatus.GameResult);
-            Assert.Equal(1, result.UserStats.Won);
-            Assert.Equal(user.Lost, result.UserStats.Lost);
-            Assert.Equal(user.Draw, result.UserStats.Draw);
+            Assert.Equal(gameResult, result.GameStatus.GameResult);
+            Assert.Equal(opponentChoice, result.GameStatus.OpponentChoice);
+            Assert.Equal(won, result.UserStats.Won);
+            Assert.Equal(lost, result.UserStats.Lost);
+            Assert.Equal(draw, result.UserStats.Draw);
             Assert.Equal("Ok.", result.Message);
         }
     }
